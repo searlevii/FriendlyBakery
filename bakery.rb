251bdcdd5ae2f51bca6a14d0e1e323b918f328aa
Savon <searle@nyc.yearup.org>
2018-12-12 15:25:47 -0500

    Linking pages

--- bakery.rb
+++ bakery.rb
@@ -3,3 +3,23 @@ require 'sinatra'
 get '/' do
     erb :home
 end
+
+get '/about.erb' do
+    erb :about
+end
+
+get '/contact.erb' do
+    erb :contact
+end
+
+get 'cakes.erb' do
+    erb :cakes
+end
+
+get 'cookies.erb' do
+    erb :cookies
+end
+
+get 'muffins.erb' do
+    erb :muffins
+end
